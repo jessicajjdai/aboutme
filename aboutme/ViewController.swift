2cefe3d6f0e950fd0477d988d4ca7489db67ca0b
jessica <jjdaichen@gmail.com>
2020-07-22 15:05:29 -0400

    Initial Commit

---
+++ aboutme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aboutme
+//
+//  Created by Jessica Dai on 7/22/20.
+//  Copyright © 2020 Jessica Dai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
